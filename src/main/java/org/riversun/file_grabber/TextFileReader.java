02a9a19c61a035068db5582c54368c3595a1d5ac
Tom Misawa <riversun.org@gmail.com>
2019-09-03 11:32:03 +0900

    update pom

--- src/main/java/org/riversun/file_grabber/TextFileReader.java
+++ src/main/java/org/riversun/file_grabber/TextFileReader.java
@@ -1,25 +1,26 @@
-/*  file-grabber Java lib for easy to handle and manipulate the text file
+/*
+ * file-grabber Java lib for easy to handle and manipulate the text file
  *
- *  Copyright (c) 2006-2016 Tom Misawa, riversun.org@gmail.com
- *  
- *  Permission is hereby granted, free of charge, to any person obtaining a
- *  copy of this software and associated documentation files (the "Software"),
- *  to deal in the Software without restriction, including without limitation
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *  and/or sell copies of the Software, and to permit persons to whom the
- *  Software is furnished to do so, subject to the following conditions:
- *  
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- *  DEALINGS IN THE SOFTWARE.
- *  
+ * Copyright (c) 2006-2016 Tom Misawa, riversun.org@gmail.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ * 
  */
 package org.riversun.file_grabber;
 
@@ -29,6 +30,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -163,6 +165,31 @@ public class TextFileReader {
    */
   public String readText(InputStream is, String charset) throws IOException {
     return readTextCharByChar(is, charset);
+  }
+
+  /**
+   * Read whole text from resource directory as UTF-8
+   * 
+   * @param fileName
+   * @param charset
+   * @return
+   * @throws IOException
+   */
+  public String readTextFromResourceAsUTF8(String fileName) throws IOException {
+    return readTextFromResource(fileName, StandardCharsets.UTF_8.toString());
+  }
+
+  /**
+   * Read whole text from resource directory
+   * 
+   * @param fileName
+   * @param charset
+   * @return
+   * @throws IOException
+   */
+  public String readTextFromResource(String fileName, String charset) throws IOException {
+    final InputStream is = TextFileReader.class.getClassLoader().getResourceAsStream(fileName);
+    return readText(is, charset);
   }
 
   /**
